03814a9f64ebd668ea4b92a2cb52d1baf6a2bbce
Nitesh <niteshk38@gmail.com>
2022-03-22 23:37:21 +0530

    Add files via upload

---
+++ 18.cpp
@@ -0,0 +1,31 @@
+/*
+    *
+   **
+  ***
+ ****
+*****
+
+*/
+
+
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cout<< "Enter the Value of N" <<endl;
+    cin >> n;
+    int i = 1;  //row
+    while (i<=n){
+
+      //Printing Star
+        int j = 1; //column
+        while (j <=n-i+1)
+        {
+            cout<<"*";
+            j = j+1;
+        } 
+        cout<<endl;
+        i = i+1;  
+    }
+}
