3559d57f203a3a553148d835276a361f3a940234
Nitesh Kumar <niteshk38@gmail.com>
2022-03-31 16:11:49 +0530

    Create Continue.cpp

---
+++ Continue.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    for(int i=0; i<=5 ;i++)
+    {
+        cout<<"Hi"<<endl;
+        cout<<"Hello"<<endl;
+
+        continue;
+        cout<<"Waiting for Message"<<endl;
+    }
+
+   
+}
