696688312ae6774a767bb9eb7f9ba755b2fed2ad
Nitesh Kumar <niteshk38@gmail.com>
2022-07-09 21:16:57 +0530

    Create ArithmeticProgression_Lec_8.cpp

---
+++ ArithmeticProgression_Lec_8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int AP(int n){
+    int sum = n*n + 7;
+    cout<<sum<<endl;
+    return 0;
+}
+
+int main() {
+    int n;
+    cout<<"Enter a Number"<<endl;
+    cin>>n;
+    AP(n);
+
+}
